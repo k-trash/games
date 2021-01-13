f12f225892f13fe1e5c31585e2d952f50a783a51
k-trash <d.kintou@gmail.com>
2021-01-13 22:25:36 +0900

    first commit

---
+++ jack_gui.h
@@ -0,0 +1,9 @@
+#pragma once
+
+GtkWidget *window;
+GtkWidget *vbox;
+GtkWidget *user_cards;
+
+void quiteButtonClicked(GtkWidget *button_, gpointer user_data_);
+
+void addCards(GtkWidget *button_, gpointer user_data_);
